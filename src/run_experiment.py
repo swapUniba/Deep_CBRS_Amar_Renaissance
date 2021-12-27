0352ceef17023783632c10c59012e6c8ef0e440b
loreloc <lorenzoloconte@outlook.it>
2021-12-27 19:22:19 +0100

    refactor datasets module to accomodate GNNs

--- src/run_experiment.py
+++ src/run_experiment.py
@@ -1,33 +1,52 @@
+import numpy as np
 from tensorflow import keras
-from models.basic import BasicCBRS
+
 from models.hybrid import HybridCBRS
-from utilities.datasets import HybridUserItemSequence, GraphUserItemSequence, BERTUserItemSequence
-from utilities.metrics import top_predictions
+from utilities.datasets import HybridUserItemEmbeddings, load_train_test_ratings
+from utilities.datasets import load_bert_user_item_embeddings, load_graph_user_item_embeddings
+from utilities.metrics import top_k_metrics
 
 
 if __name__ == '__main__':
-    dataset = HybridUserItemSequence(
-        'datasets/movielens/train2id.tsv',
+    # Load train and test ratings
+    (train_ratings, test_ratings), (users, items) = load_train_test_ratings(
+        'datasets/movielens/train2id.csv',
+        'datasets/movielens/test2id.csv'
+    )
+
+    # Load both Graph and BERT user/item embeddings
+    graph_user_embeddings, graph_item_embeddings = load_graph_user_item_embeddings(
         'embeddings/user-item-properties/768DistMult.json',
+        users, items
+    )
+    bert_user_embeddings, bert_item_embeddings = load_bert_user_item_embeddings(
         'embeddings/bert/user-lastlayer.json',
         'embeddings/bert/item-lastlayer.json',
+        users, items
+    )
+
+    # Setup both training and test data sequences
+    data_train = HybridUserItemEmbeddings(
+        train_ratings, graph_user_embeddings, graph_item_embeddings, bert_user_embeddings, bert_item_embeddings,
         batch_size=512, shuffle=True
     )
-
-    model = HybridCBRS(feature_based=False)
-    optimizer = keras.optimizers.Adam(learning_rate=1e-3, beta_1=0.9)
-    model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
-
-    model.fit(dataset, epochs=25, workers=4)
-
-    dataset = HybridUserItemSequence(
-        'datasets/movielens/test2id.tsv',
-        'embeddings/user-item-properties/768DistMult.json',
-        'embeddings/bert/user-lastlayer.json',
-        'embeddings/bert/item-lastlayer.json',
-        evaluate=True, batch_size=512
+    data_test = HybridUserItemEmbeddings(
+        test_ratings, graph_user_embeddings, graph_item_embeddings, bert_user_embeddings, bert_item_embeddings,
+        batch_size=2048, shuffle=False
     )
 
-    predictions = model.predict(dataset, workers=4)
-    top_predictions = top_predictions(predictions, dataset.get_user_ids(), dataset.get_item_ids(), k=5)
-    print(top_predictions)
+    # Instantiate, train and evaluate a hybrid feature-based recommender system
+    model = HybridCBRS(feature_based=True)
+    optimizer = keras.optimizers.Adam(learning_rate=1e-3)
+    model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+    model.fit(data_train, epochs=25)
+    model.evaluate(data_test)
+
+    # Compute Precision, Recall and F1 @K metrics
+    predictions = model.predict(data_test)
+    ratings_pred = np.concatenate([test_ratings[:, [0, 1]], predictions], axis=1)
+    print(top_k_metrics(test_ratings, ratings_pred, k= 1))
+    print(top_k_metrics(test_ratings, ratings_pred, k= 3))
+    print(top_k_metrics(test_ratings, ratings_pred, k= 5))
+    print(top_k_metrics(test_ratings, ratings_pred, k=10))
+    print(top_k_metrics(test_ratings, ratings_pred, k=15))
--- src/utilities/datasets.py
+++ src/utilities/datasets.py
@@ -5,7 +5,7 @@ import pandas as pd
 from tensorflow import keras
 
 
-class BaseUserItemSequence(keras.utils.Sequence):
+class UserItemEmbeddings(keras.utils.Sequence):
     def __init__(
         self,
         ratings,
@@ -26,29 +26,26 @@ class BaseUserItemSequence(keras.utils.Sequence):
         :param seed: The seed value used to shuffle the sequence.
         """
         super().__init__()
+
+        # Set the ratings
         self.ratings = ratings
+
+        # Set the embeddings
         self.user_embeddings = user_embeddings
         self.item_embeddings = item_embeddings
+
+        # Set other settings
         self.batch_size = batch_size
         self.shuffle = shuffle
-        self.random_state = np.random.RandomState(seed)
+        self.seed = seed
         self.indexes = None
+        self.random_state = None
         self.on_epoch_end()
 
-    def get_user_ids(self):
-        """
-        Get the an array of user ids.
-
-        :return: An array of user ids.
-        """
+    def get_users(self):
         return np.unique(self.ratings[:, 0])
 
-    def get_item_ids(self):
-        """
-        Get the an array of item ids.
-
-        :return: An array of item ids.
-        """
+    def get_items(self):
         return np.unique(self.ratings[:, 1])
 
     def __len__(self):
@@ -63,166 +60,179 @@ class BaseUserItemSequence(keras.utils.Sequence):
         """
         Get the i-th batch consisting of User-Item embeddings and the rating.
 
-        :param idx: THe index of the batch.
+        :param idx: The index of the batch.
         :return: A pair consisting of User-Item embeddings and the ratings.
         """
         batch_idx = idx * self.batch_size
-        batch_off = min(batch_idx + self.batch_size, len(self.indexes))
-        indexes = self.indexes[batch_idx:batch_off]
-        ratings = self.ratings[indexes]
-        user_embeddings = np.stack([self.user_embeddings[u] for u in ratings[:, 0]])
-        item_embeddings = np.stack([self.item_embeddings[i] for i in ratings[:, 1]])
+        batch_off = min(batch_idx + self.batch_size, len(self.ratings))
+        if self.shuffle:
+            ratings = self.ratings[self.indexes[batch_idx:batch_off]]
+        else:
+            ratings = self.ratings[batch_idx:batch_off]
+        user_embeddings = self.user_embeddings[ratings[:, 0]]
+        item_embeddings = self.item_embeddings[ratings[:, 1]]
         return (user_embeddings, item_embeddings), ratings[:, 2]
 
     def on_epoch_end(self):
         """
         Shuffles the indexes at the end of every epoch.
         """
-        self.indexes = np.arange(len(self.ratings))
         if self.shuffle:
+            if self.random_state is None:
+                self.random_state = np.random.RandomState(self.seed)
+            self.indexes = np.arange(len(self.ratings))
             self.random_state.shuffle(self.indexes)
 
 
-class GraphUserItemSequence(BaseUserItemSequence):
+class HybridUserItemEmbeddings(keras.utils.Sequence):
     def __init__(
         self,
-        ratings_filepath,
-        graph_filepath,
-        evaluate=False,
+        ratings,
+        graph_user_embeddings,
+        graph_item_embeddings,
+        bert_user_embeddings,
+        bert_item_embeddings,
         batch_size=512,
         shuffle=False,
         seed=42
     ):
-        """
-        Initialize a User-Item embeddings sequence with Graph embeddings.
-
-        :param ratings_filepath: The filepath of the ratings file.
-        :param graph_filepath: The filepath of user and item Graph embeddings.
-        :param evaluate: Whether to enable evaluation mode, i.e. produces all the combinations of User-Item.
-        :param batch_size: The batch size.
-        :param shuffle: Whether to shuffle the sequence.
-        :param seed: The seed value used to shuffle the sequence.
-        """
-        ratings = load_ratings(ratings_filepath, evaluate=evaluate)
-        embeddings = load_graph_user_item_embeddings(graph_filepath)
-        super().__init__(
-            ratings, embeddings, embeddings,
-            batch_size=batch_size, shuffle=shuffle and not evaluate, seed=seed
+        self.graph_embeddings = UserItemEmbeddings(
+            ratings, graph_user_embeddings, graph_item_embeddings,
+            batch_size=batch_size, shuffle=shuffle, seed=seed
         )
-
-
-class BERTUserItemSequence(BaseUserItemSequence):
-    def __init__(
-        self,
-        ratings_filepath,
-        user_filepath,
-        item_filepath,
-        evaluate=False,
-        batch_size=512,
-        shuffle=False,
-        seed=42
-    ):
-        """
-        Initialize a User-Item embeddings sequence with BERT embeddings.
-
-        :param ratings_filepath: The filepath of the ratings file.
-        :param user_filepath: The filepath of user BERT embeddings.
-        :param item_filepath: The filepath of item BERT embeddings.
-        :param evaluate: Whether to enable evaluation mode, i.e. produces all the combinations of User-Item.
-        :param batch_size: The batch size.
-        :param shuffle: Whether to shuffle the sequence.
-        :param seed: The seed value used to shuffle the sequence.
-        """
-        ratings = load_ratings(ratings_filepath, evaluate=evaluate)
-        user_embeddings, item_embeddings = load_bert_user_item_embeddings(user_filepath, item_filepath)
-        super().__init__(
-            ratings, user_embeddings, item_embeddings,
-            batch_size=batch_size, shuffle=shuffle and not evaluate, seed=seed
+        self.bert_embeddings = UserItemEmbeddings(
+            ratings, bert_user_embeddings, bert_item_embeddings,
+            batch_size=batch_size, shuffle=shuffle, seed=seed
         )
-
-
-class HybridUserItemSequence(keras.utils.Sequence):
-    def __init__(
-        self,
-        ratings_filepath,
-        graph_filepath,
-        user_filepath,
-        item_filepath,
-        evaluate=False,
-        batch_size=512,
-        shuffle=False,
-        seed=42
-    ):
-        """
-        Initialize a User-Item embeddings sequence with both Graph and BERT embeddings.
-
-        :param ratings_filepath: The filepath of the ratings file.
-        :param graph_filepath: The filepath of user and item Graph embeddings.
-        :param user_filepath: The filepath of user BERT embeddings.
-        :param item_filepath: The filepath of item BERT embeddings.
-        :param evaluate: Whether to enable evaluation mode, i.e. produces all the combinations of User-Item.
-        :param batch_size: The batch size.
-        :param shuffle: Whether to shuffle the sequence.
-        :param seed: The seed value used to shuffle the sequence.
-        """
-        ratings = load_ratings(ratings_filepath, evaluate=evaluate)
-        graph_embeddings = load_graph_user_item_embeddings(graph_filepath)
-        user_embeddings, item_embeddings = load_bert_user_item_embeddings(user_filepath, item_filepath)
-        self.graph_sequence = BaseUserItemSequence(
-            ratings, graph_embeddings, graph_embeddings,
-            batch_size=batch_size, shuffle=shuffle and not evaluate, seed=seed
-        )
-        self.bert_sequence = BaseUserItemSequence(
-            ratings, user_embeddings, item_embeddings,
-            batch_size=batch_size, shuffle=shuffle and not evaluate, seed=seed
-        )
-
-    def get_user_ids(self):
-        """
-        Get the an array of user ids.
-
-        :return: An array of user ids.
-        """
-        return self.graph_sequence.get_user_ids()
-
-    def get_item_ids(self):
-        """
-        Get the an array of item ids.
-
-        :return: An array of item ids.
-        """
-        return self.graph_sequence.get_item_ids()
 
     def __len__(self):
         """
-        Get the number of full batches.
+        Get the number of batches.
 
-        :return: The number of full batches.
+        :return: The number of batches.
         """
-        return len(self.graph_sequence)
+        return len(self.graph_embeddings)
 
     def __getitem__(self, idx):
         """
-        Get the i-th batch consisting of User-Item Graph and BERT embeddings, and the rating.
+        Get the i-th batch consisting of User-Item embeddings and the rating.
 
-        :param idx: THe index of the batch.
-        :return: A tuple consisting of User-Item Graph and BERT embeddings and the ratings.
+        :param idx: The index of the batch.
+        :return: A pair consisting of User-Item embeddings and the ratings.
         """
-        (user_graph_embeddings, item_graph_embeddings), ratings = self.graph_sequence[idx]
-        (user_bert_embeddings, item_bert_embeddings), ratings1 = self.bert_sequence[idx]
+        (user_graph_embeddings, item_graph_embeddings), ratings = self.graph_embeddings[idx]
+        (user_bert_embeddings, item_bert_embeddings), _ = self.bert_embeddings[idx]
         return (user_graph_embeddings, item_graph_embeddings, user_bert_embeddings, item_bert_embeddings), ratings
+
+    def on_epoch_end(self):
+        """
+        Calls on_epoch_end() to any sub-sequence.
+        """
+        self.graph_embeddings.on_epoch_end()
+        self.bert_embeddings.on_epoch_end()
+
+
+class UserItemGraph(keras.utils.Sequence):
+    def __init__(
+        self,
+        ratings,
+        adj_matrix,
+        batch_size=512,
+        shuffle=False,
+        seed=42
+    ):
+        super().__init__()
+
+        # Set the ratings and the adjency matrix
+        self.ratings = ratings
+        self.adj_matrix = adj_matrix
+
+        # Set other settings
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.seed = seed
+        self.indexes = None
+        self.random_state = None
+        self.on_epoch_end()
+
+    def get_users(self):
+        return np.unique(self.ratings[:, 0])
+
+    def get_items(self):
+        return np.unique(self.ratings[:, 1])
+
+    def __len__(self):
+        """
+        Get the number of batches.
+
+        :return: The number of batches.
+        """
+        return int(np.ceil(len(self.ratings) / self.batch_size))
+
+    def __getitem__(self, idx):
+        """
+        Get the i-th batch consisting of User-Item embeddings and the rating.
+
+        :param idx: The index of the batch.
+        :return: A pair consisting of User-Item embeddings and the ratings.
+        """
+        batch_idx = idx * self.batch_size
+        batch_off = min(batch_idx + self.batch_size, len(self.ratings))
+        if self.shuffle:
+            ratings = self.ratings[self.indexes[batch_idx:batch_off]]
+        else:
+            ratings = self.ratings[batch_idx:batch_off]
+        return (ratings[:, 0], ratings[:, 1], self.adj_matrix), ratings[:, 2]
 
     def on_epoch_end(self):
         """
         Shuffles the indexes at the end of every epoch.
         """
-        self.graph_sequence.on_epoch_end()
-        self.bert_sequence.on_epoch_end()
+        if self.shuffle:
+            if self.random_state is None:
+                self.random_state = np.random.RandomState(self.seed)
+            self.indexes = np.arange(len(self.ratings))
+            self.random_state.shuffle(self.indexes)
 
 
-def load_bert_embeddings(filepath):
-    embeddings = pd.read_json(filepath)
-    return embeddings.sort_values(by=['ID_OpenKE'])
+def load_train_test_ratings(train_filepath, test_filepath, return_adjacency=False):
+    """
+    Load train and test ratings. Note that the user and item IDs are converted to sequential numbers.
+
+    :param train_filepath: The training ratings CSV filepath.
+    :param test_filepath: The test ratings CSV filepath.
+    :return: The training and test ratings as an array of User-Item-Rating where IDs are made sequential.
+             Moreover, it returns the users and items original unique IDs if return_adjacency is False,
+             otherwise it returns the training interactions adjacency matrix (assuming un-directed arcs).
+    """
+    # Load the ratings arrays
+    train_ratings = pd.read_csv(train_filepath).to_numpy()
+    test_ratings = pd.read_csv(test_filepath).to_numpy()
+
+    # Convert users and items ids to indices (i.e. sequential)
+    users, users_indexes = np.unique(train_ratings[:, 0], return_inverse=True)
+    items, items_indexes = np.unique(train_ratings[:, 1], return_inverse=True)
+    train_ratings = np.stack([users_indexes, items_indexes, train_ratings[:, 2]], axis=1)
+
+    # Do the same for the test ratings, by using the same users and items of the train ratings
+    users_indexes = np.argwhere(test_ratings[:, [0]] == users)[:, 1]
+    items_indexes = np.argwhere(test_ratings[:, [1]] == items)[:, 1]
+    test_ratings = np.stack([users_indexes, items_indexes, test_ratings[:, 2]], axis=1)
+
+    if not return_adjacency:
+        return (train_ratings, test_ratings), (users, items)
+
+    # Add a constant factors to item ids
+    train_ratings[:, 1] += len(users)
+    test_ratings[:, 1] += len(users)
+
+    # Compute the adjacency matrix
+    adj_size = len(users) + len(items)
+    adj_matrix = np.zeros((adj_size, adj_size), dtype=np.int64)
+    adj_matrix[train_ratings[:, 0], train_ratings[:, 1]] = 1
+    adj_matrix += adj_matrix.T
+
+    return (train_ratings, test_ratings), adj_matrix
 
 
 def load_graph_embeddings(filepath):
@@ -231,7 +241,19 @@ def load_graph_embeddings(filepath):
     return embeddings['ent_embeddings']
 
 
-def load_bert_user_item_embeddings(user_filepath, item_filepath):
+def load_bert_embeddings(filepath):
+    embeddings = pd.read_json(filepath)
+    return embeddings.sort_values(by=['ID_OpenKE'])
+
+
+def load_graph_user_item_embeddings(filepath, users, items):
+    graph_embeddings = np.array(load_graph_embeddings(filepath), dtype=np.float32)
+    user_embeddings = graph_embeddings[users]
+    item_embeddings = graph_embeddings[items]
+    return user_embeddings, item_embeddings
+
+
+def load_bert_user_item_embeddings(user_filepath, item_filepath, users, items):
     user_embeddings, item_embeddings = dict(), dict()
     df_users = load_bert_embeddings(user_filepath)
     df_items = load_bert_embeddings(item_filepath)
@@ -241,31 +263,6 @@ def load_bert_user_item_embeddings(user_filepath, item_filepath):
     for _, item in df_items.iterrows():
         item_id = item['ID_OpenKE']
         item_embeddings[item_id] = np.array(item['embedding'], dtype=np.float32)
+    user_embeddings = np.stack([user_embeddings[u] for u in users])
+    item_embeddings = np.stack([item_embeddings[i] for i in items])
     return user_embeddings, item_embeddings
-
-
-def load_graph_user_item_embeddings(filepath):
-    return np.array(load_graph_embeddings(filepath), dtype=np.float32)
-
-
-def load_ratings(filepath, evaluate=False):
-    """
-    Load binary ratings from file.
-
-    :param filepath: The filepath.
-    :param evaluate: Whether to load only user and item ids.
-    :return: If evaluate is True, then an array of all the possible combination of user and item ids
-     (with -1 as score) is returned. Hoterwise, an array of triples User-Item-Rating, where Rating is either 0 or 1.
-    """
-    ratings = []
-    with open(filepath) as fp:
-        csv_reader = csv.reader(fp, delimiter='\t')
-        for row in csv_reader:
-            ratings.append((int(row[0]), int(row[1]), int(row[2])))
-    ratings = np.array(ratings, dtype=np.int32)
-
-    if evaluate:
-        user_ids, item_ids = np.unique(ratings[:, 0]), np.unique(ratings[:, 1])
-        ratings = np.reshape(np.stack(np.meshgrid(user_ids, item_ids)).T, [-1, 2])
-        return np.concatenate([ratings, np.full((len(ratings), 1), -1, dtype=np.int32)], axis=1)
-    return ratings
