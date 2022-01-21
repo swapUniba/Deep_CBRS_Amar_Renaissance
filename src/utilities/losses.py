2b4cbacab15cac7f7b03ff65cdc3d83b9b9e919b
Pasquale De Marinis <pasquale199876@hotmail.it>
2022-01-21 16:57:36 +0100

    Added BPR loss

---
+++ src/utilities/losses.py
@@ -0,0 +1,17 @@
+from tensorflow import keras
+import tensorflow as tf
+
+
+class BPRLoss(keras.losses.Loss):
+    def __init__(self, name="BPR_loss"):
+        super().__init__(name=name)
+
+    def call(self, y_true, y_pred):
+        half = len(y_pred) // 2
+        return - tf.reduce_sum(
+            tf.math.log(
+                tf.math.sigmoid(
+                    y_pred[0:half] - y_pred[half+1::]
+                )
+            )
+        )
