b22101dc5bf08d013c451652232f27cdf731ea99
Pasquale De Marinis <pasquale199876@hotmail.it>
2022-01-24 22:09:13 +0100

    TwoWayGNNs and Hybrid experiments

--- src/experiment.py
+++ src/experiment.py
@@ -27,7 +27,7 @@ import mlflow
 PARAMS_PATH = 'config.yaml'
 EXPERIMENTS_PATH = 'experiments.yaml'
 MLFLOW_PATH = './mlruns'
-MLFLOW_EXP_NAME = 'SIS - Movielens-1M - BasicRS with Knowledge GNNs prova'
+MLFLOW_EXP_NAME = 'SIS - Movielens-1M - HybridRS with GNNs'
 LOG_FREQUENCY = 100
 METRICS_TOP_KS = [5, 10]
 os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
